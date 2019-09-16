ce12cc3e57c485b0951b882e639a2640190d51e5
probe <probe.park@snowcorp.com>
2019-09-17 01:06:48 +0900

    Add a ui for displaying messages

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -13,12 +13,116 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final _messages = <ChatMessage>[];
+  final _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('FriendlyChat'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: SafeArea(child: _buildTextComposer())),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration:
+                      InputDecoration.collapsed(hintText: 'Send a message'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    if (text.isEmpty) {
+      return;
+    }
+    _textController.clear();
+    final message = ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+
+  const ChatMessage({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const _name = "Probe";
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: new Text(_name[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text))
+            ],
+          )
+        ],
       ),
     );
   }
